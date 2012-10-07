0a9d1d670294bf917ccb7450f3e1cd6635b7e998
Aditya Mukherji <aditya.mukherji@gmail.com>
2012-10-07 13:26:07 -0700

    Initial Rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wikibrowse::Application.config.secret_token = '99b71f19f19c0dbd615607c5c15cd6601be833d5f0b625af6af4bce5792618c3aa3d9d6919ec50f7aaf551ad40ac9897e88d678c3f0b28556a6d9a392c1dc154'
